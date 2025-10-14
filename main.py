919aa7e546bdc4dd36a9001469f39b87284c54e1
M.L. Cole <marcuscole@protonmail.com>
2025-10-14 15:06:57 -0500

    Initial Commit.
    Create functional version that allows for MAC address formatting individually or in lists, then returns the object back to the user's clipboard ready for pasting.

---
+++ main.py
@@ -0,0 +1,67 @@
+import pyperclip
+
+def list_sorter(listOfMACs, delimiter):
+    results = []
+    print(f"List Detected: '{listOfMACs}'")
+    print(f"Delimiter detected: '{delimiter}'")
+    print("Processing List...")
+    listOfMACs = listOfMACs.strip()
+    listOfMACs = listOfMACs.split(delimiter)
+    mac: str
+    for mac in listOfMACs:
+        mac = mac.strip()
+        if not mac:
+            continue
+        processedMAC = processor(mac)
+        uppercaseMAC = processedMAC.upper()
+        results.append(uppercaseMAC)
+    if delimiter == ",":
+        joiner = delimiter + ' '
+        joined = joiner.join(results)
+    else:
+        joined = delimiter.join(results)
+    print(f"New List Created: {joined}")
+    pyperclip.copy(joined)
+    return results
+
+def processor(mac_address):
+    if ':' in mac_address:
+        processedMAC = colon_stripper(mac_address)
+        return processedMAC
+
+    else:
+        if len(mac_address) % 2 == 0:
+            processedMAC = coloner(mac_address)
+            return processedMAC
+        else:
+            print("Please try again. An even number of characters is required.")
+            return None
+
+def colon_stripper(mac_address):
+    target_stripped = mac_address.replace(":", "")
+    processedMAC = target_stripped
+    return processedMAC
+
+def coloner(mac_address):
+    target_coloned = ':'.join(mac_address[char:char+2] for char in range(0, len(mac_address), 2))
+    processedMAC = target_coloned
+    return processedMAC
+
+
+while True:
+    target = input("Please provide the MAC address you'd like processed (or enter 'x' to eXit): ")
+
+    if target == "X" or target == "x" or target == "exit":
+        exit()
+    # Search for List Delimiters & send to sorter for list formatting
+    elif '\n' in target:
+        list_sorter(target, delimiter='\n')
+    elif ',' in target:
+        list_sorter(target, delimiter=',')
+    elif ' ' in target:
+        list_sorter(target, delimiter=' ')
+    # Process Individual MAC addresses
+    else:
+        processedMAC = processor(target)
+        print(f"{processedMAC} added to user clipboard.")
+        pyperclip.copy(processedMAC)
